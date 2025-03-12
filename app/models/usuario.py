1072c83efe024a75fe3d88a959af72b4ae989860
tife502 <125226019+tife502@users.noreply.github.com>
2025-03-12 18:02:27 -0500

    Debug Usuario

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -7,8 +7,8 @@ class Usuario(db.Model):
     nombre = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    rol_id = db.Column(db.Integer, db.ForeignKey('rol.id'), nullable=False, default = 3)
-    rol_id = db.relationship('Rol', backref=db.backref('usuarios', lazy=True))
+    rol_id = db.Column(db.Integer, db.ForeignKey('Rol.id'), nullable=False, default = 3)
+    rol = db.relationship('Rol', backref=db.backref('usuarios', lazy=True))
 
 
     def to_dict(self):
