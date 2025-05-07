2ab50ce6734e83a5feec5fb5b0bbdf1e8966614b
Solau03 <150872507+Solau03@users.noreply.github.com>
2025-05-06 21:13:27 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -11,6 +11,6 @@ class Config:
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://usuario:password@localhost:5432/gestion_obras")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestion_obras_user:GRMyMNzYVSV6agxrbG6EQYYZ48ws73Lu@dpg-d0dbfgjuibrs73f5mqvg-a.oregon-postgres.render.com/gestion_obras")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
